06824d324872ff9c2e203bfe8066f2d4eb5155b5
Muhammed Balıkçı <muhammed.balikci@id3.com.tr>
2024-08-22 13:55:39 +0300

    Add project files.

---
+++ bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,102 @@
+﻿using bookDemo.Data;
+using bookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContex.Books.ToList();
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBooks([FromRoute(Name = "id")] int id)
+        {
+            var books = ApplicationContex.Books.Where(x => x.Id.Equals(id)).SingleOrDefault();
+
+            if (books == null)
+                return NotFound();//404
+
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest();
+                ApplicationContex.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UptadeOneBook(int id, Book book)
+        {
+            var entity = ApplicationContex.Books.FirstOrDefault(x => x.Id == id);
+
+
+            if (entity is null)
+                return NotFound();
+
+            if (id != book.Id)
+                return BadRequest();
+
+            ApplicationContex.Books.Remove(entity);
+            book.Id = entity.Id;
+            ApplicationContex.Books.Add(book);
+            return NoContent();
+
+        }
+
+        [HttpDelete("OneBook")]
+        public IActionResult DeleteOneBook(int id, Book book)
+        {
+            var entity = ApplicationContex.Books.FirstOrDefault(x => x.Id == id);
+
+            if (entity is null)
+                return NotFound();
+
+            if (id != book.Id)
+                return BadRequest();
+
+            ApplicationContex.Books.Remove(entity);
+            return Ok(book);
+
+        }
+
+        [HttpDelete("AllBooks")]
+        public IActionResult DeleteAllBook()
+        {
+            ApplicationContex.Books.Clear();
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+            var entity=ApplicationContex.Books.Find(x => x.Id == id);
+
+            if (entity is null)
+                return NotFound();
+
+            bookPatch.ApplyTo(entity);
+            return NoContent();
+        }
+    }
+}
---
+++ bookDemo/Data/ApplicationContex.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+namespace bookDemo.Data
+{
+    public static class ApplicationContex
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContex()
+        {
+
+            Books = new List<Book>()
+            {
+                new Book(){ Id = 1, Title = "Karagöz ve Hacivat", Price = 70 },
+                new Book(){ Id = 2, Title = "Mesnevi", Price = 150 },
+                new Book(){ Id = 3, Title = "Dede Korkut", Price = 70 }
+            };
+        }
+    }
+}
